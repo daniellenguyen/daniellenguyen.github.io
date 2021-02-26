24300fcbd577d963d805e61709fc2c3cd69282c3
daniellenguyen <daniellenguyen77@gmail.com>
2021-02-26 17:43:32 -0500

    added link to policing vis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {lazy, Component, Suspense} from 'react'
+import {importMDX} from 'mdx.macro'
+const Content = lazy(() => importMDX('./Content.mdx'))
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Content />
+        </Suspense>
+      </div>
+    )
+  }
 }
-
-export default App;
